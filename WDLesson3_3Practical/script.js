76b41ceae14c4d894828e695ad4f7dc14460c779
Calvinh46 <calvinh46@nycstudents.net>
2026-01-06 18:18:39 +0000

    added

---
+++ WDLesson3_3Practical/script.js
@@ -0,0 +1,20 @@
+/* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function goanimate1(){
+    document.getElementById("highlight").innerHTML = "Go!Animate 2007-2010";
+}
+
+function goanimate2(){
+    document.getElementById("highlight").innerHTML = "Go!Animate 2010-2013";
+}
+
+function goanimate3(){
+    document.getElementById("highlight").innerHTML = "Go!Animate 2013-2018";
+}
+
+function goanimate4(){
+    document.getElementById("highlight").innerHTML = "Go!Animate 4 Schools 2011-2019";
+}
+
+function vyond(){
+    document.getElementById("highlight").innerHTML = "Vyond 2018-Present";
+}
