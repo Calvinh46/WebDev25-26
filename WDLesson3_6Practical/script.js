69e07966463cee9233d97bb95e4844f15bc3b58a
Calvinh46 <calvinh46@nycstudents.net>
2026-01-22 17:49:03 +0000

    modded2

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -3,9 +3,9 @@ function avg(){
     let g1 = parseInt(document.getElementById("grade1").value);
     let g2 = parseInt(document.getElementById("grade2").value);
     let g3 = parseInt(document.getElementById("grade3").value);
-    let op = document.getElementById("output");
+    let op1 = document.getElementById("output1");
     let avg = (g1 + g2 + g3) / 3;
-    op.innerHTML =`The average of all 3 grades is ${avg}`;
+    op1.innerHTML =`The average of all 3 grades is ${avg}`;
 
 }
 
@@ -15,9 +15,9 @@ function slope(){
     let y2 = parseInt(document.getElementById("y2").value);
     let x1 = parseInt(document.getElementById("x1").value);
     let x2 = parseInt(document.getElementById("x2").value);
-    let op = document.getElementById("output");
+    let op2 = document.getElementById("output2");
     let m = (y2-y1)/(x2-x1);
-    op.innerHTML = `The slope of the line between (${x2},${x1}) and (${y2},${y1}) is ${m}`;
+    op2.innerHTML = `The slope of the line between (${x2},${x1}) and (${y2},${y1}) is ${m}`;
 
 }
 
@@ -25,7 +25,7 @@ function slope(){
 function BMI(){
     let height = parseFloat(document.getElementById("ht").value);
     let weight = parseFloat(document.getElementById("wt").value);
-    let op = document.getElementById("output");
+    let op3 = document.getElementById("output3");
     let bmi = 703 * weight/(height*height);
-    op.innerHTML = `Your BMI is ${bmi}`;
+    op3.innerHTML = `Your BMI is ${bmi}`;
 }
