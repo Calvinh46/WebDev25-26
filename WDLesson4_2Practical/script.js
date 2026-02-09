74eebaa1bfaa6f2b3425b0cb3e7b800c83b8b3d9
Calvinh46 <calvinh46@nycstudents.net>
2026-02-09 18:05:16 +0000

    added

---
+++ WDLesson4_2Practical/script.js
@@ -0,0 +1,61 @@
+// General Guideline
+/*
+function name(){
+  // Step 1: Create variables for the information from the text inputs and the output element
+
+  // Step 2: Complete the process
+  //    a) Perform any calculations necessary
+  //    b) Perform necessary decisions
+
+  // Step 3: Display the output
+
+}
+*/
+/* Challenge 2:  Create the function for Challenge 1 scenario */
+function calcArea(){
+  let l = parseFloat(document.getElementById("l").value);
+  let w = parseFloat(document.getElementById("w").value);
+  let op = document.getElementById("output");
+  let A = l * w
+  let message = " "
+
+  if (l <= 0 || w <= 0){
+      message = ("Inappropiate Measurement")    
+  }else{
+      message = `The area within the length ${l} and width ${w} is: ${A} sq.units.`;
+  }
+
+  op.innerHTML = `${message}`;
+}
+
+
+/* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
+1) Review the HTML for the UI.  Remember dropdown boxes are just input.
+2) You will need to write HTML as part of the output.  
+   For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
+function pet(){
+  let a = document.getElementById("animal");
+  let e = document.getElementById("emotion");
+  let output = document.getElementById("output");
+  let filename = ""
+  if (a == "Bear" && e == "Funny"){
+    filename = "funnyBear.jpg";
+  }
+  if (a == "Bear" && e == "Sad"){
+    filename = "sadBear.jpg";
+  }
+  if (a == "Cat" && e == "Funny"){
+    filename = "funnyCat.jpg";
+  }
+  if (a == "Cat" && e == "Sad"){
+    filename = "sadCat.jpg";
+  }
+  if (a == "Dog" && e == "Funny"){
+    filename = "funnyDog.jpg";
+  }
+  if (a == "Dog" && e == "Sad"){
+    filename = "sadDog.jpg";
+  }
+
+  output.innerHTML = `<img src="${filename}">`;
+}
